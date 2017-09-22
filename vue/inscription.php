ccf8164cb1ff15a0edbd865208cbd9f93da2ad7e
Leila.L <leila.lababsa@hotmail.fr>
2017-09-22 12:14:27 +0200

    create header+footer+registration page + connection page

---
+++ vue/inscription.php
@@ -0,0 +1,44 @@
+<?php
+include("template/header.php");
+ ?>
+
+ <div class="row" style="margin-bottom: 30px; margin-top:30px;">
+   <form class="col s12">
+     <!-- <div class="row"> -->
+       <div class="input-field col s10">
+         <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+         <label for="first_name">Pseudo</label>
+       </div>
+
+     <!-- <div class="row"> -->
+       <div class="input-field col s10">
+         <input id="password" type="password" class="validate">
+         <label for="password">Password</label>
+       </div>
+     <!-- </div> -->
+
+     <!-- <div class="row"> -->
+       <div class="input-field col s10">
+         <input id="password" type="password" class="validate">
+         <label for="password">Confirmation Password</label>
+       </div>
+     <!-- </div> -->
+
+     <!-- <div class="row"> -->
+       <div class="input-field col s10">
+         <input id="email" type="email" class="validate">
+         <label for="email">Email</label>
+       </div>
+     <!-- </div> -->
+     <div class="input-field col s6">
+        <input class="waves-effect btn light-blue darken-1" value="Envoyer" type="submit" name="envoie">
+      </div>
+
+   </form>
+ </div>
+
+
+
+ <?php
+ include("template/footer.php");
+  ?>
---
+++ vue/template/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TP Espace Membres</title>
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <link rel="icon" href="favicon.png">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body>
+
+
+<header>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">1</a></li>
+        <li><a href="connexion.php">Connexion</a></li>
+        <li><a href="#">3</a></li>
+      </ul>
+    </div>
+  </nav>
+</header>
